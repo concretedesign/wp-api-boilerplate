05cb88cf21c02d2cb81e10582f1d50fd29ab34b4
Matt Crider <mattcrider@gmail.com>
2016-03-05 11:35:28 -0500

    Removed more theme cruft

--- wp-content/themes/api/functions.php
+++ wp-content/themes/api/functions.php
@@ -109,60 +109,9 @@ function api_content_width() {
 add_action( 'after_setup_theme', 'api_content_width', 0 );
 
 /**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
-function api_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'api' ),
-		'id'            => 'sidebar-1',
-		'description'   => '',
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
-}
-add_action( 'widgets_init', 'api_widgets_init' );
-
-/**
  * Enqueue scripts and styles.
  */
 function api_scripts() {
 	wp_enqueue_style( 'api-style', get_stylesheet_uri() );
-
-	wp_enqueue_script( 'api-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
-	wp_enqueue_script( 'api-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
 }
 add_action( 'wp_enqueue_scripts', 'api_scripts' );
-
-/**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
- * Custom template tags for this theme.
- */
-require get_template_directory() . '/inc/template-tags.php';
-
-/**
- * Custom functions that act independently of the theme templates.
- */
-require get_template_directory() . '/inc/extras.php';
-
-/**
- * Customizer additions.
- */
-require get_template_directory() . '/inc/customizer.php';
-
-/**
- * Load Jetpack compatibility file.
- */
-require get_template_directory() . '/inc/jetpack.php';
